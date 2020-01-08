3a1ef50459045b1d3d3789cdffc45aab5fb6bb3b
Godefroid Chapelle <gotcha@bubblenet.be>
2020-01-08 15:26:23 +0100

    complete options
    color
    working-directory
    justfile
    show
    shell
    shell-args
    set

--- just-completion.bash
+++ just-completion.bash
@@ -5,15 +5,49 @@ _just_completions()
 
   COMPREPLY=()   # Array variable storing the possible completions.
   cur=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
+  if (( COMP_CWORD > 1 )); then
+    pprev=${COMP_WORDS[COMP_CWORD-2]}
+    case "$pprev" in
+      --set*)
+       COMPREPLY=();
+       return 0;;
+    esac
+  fi
+
+  case "$prev" in
+    --color*)
+     COMPREPLY=( $( compgen -W 'auto always never' "$cur"));
+     return 0;;
+    -d|--working-directory*)
+     COMPREPLY=( $( compgen -d "$cur"));
+     return 0;;
+    -f|--justfile*)
+     COMPREPLY=( $( compgen -f "$cur"));
+     return 0;;
+    -s|--show*)
+     COMPREPLY=($(compgen -W "$(just --summary)" "$cur"));
+     return 0;;
+    --set|--shell-arg*)
+     COMPREPLY=();
+     return 0;;
+    --shell*)
+     COMPREPLY=( $( compgen -c "$cur"));
+     return 0;;
+  esac
 
   case "$cur" in
-    -*)
-    COMPREPLY=( $( compgen -W '-e -l -q -v -h -V \
-                               --edit --summary --version --verbose \
+    --*)
+    COMPREPLY=( $( compgen -W '--edit --summary --version --verbose \
                                --dry-run --dump --clear-shell-args \
                                --evaluate --highlight --init --list \
-                               --no-highlight --quiet --help' -- $cur ) );;
-#   Generate the completion matches and load them into $COMPREPLY array.
+                               --no-highlight --quiet --help --color \
+                               --justfile --set --shell --shell-arg \
+                               --show --working-directory \
+                              ' -- $cur ) );;
+    -*)
+    COMPREPLY=( $( compgen -W '-e -l -q -v -h -V -f -s -d -- \
+                              ' -- $cur ) );;
     *)
     COMPREPLY=($(compgen -W "$(just --summary)" "$cur"));;
   esac
@@ -22,3 +56,7 @@ _just_completions()
 }
 
 complete -F _just_completions just
+
+
+        # --shell <SHELL>                            Invoke <SHELL> to run recipes [default: sh]
+        # --shell-arg <SHELL-ARG>...                 Invoke shell with <SHELL-ARG> as an argument [default: -cu]
