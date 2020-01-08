88ff10e940c2754b94c2145e06373d375820bb63
Godefroid Chapelle <gotcha@bubblenet.be>
2020-01-08 15:13:48 +0100

    complete flags

--- just-completion.bash
+++ just-completion.bash
@@ -1,8 +1,24 @@
 #/usr/bin/env bash
 _just_completions()
 {
-  COMPREPLY=($(compgen -W "$(just --summary)" "${COMP_WORDS[${COMP_CWORD}]}"))
+  local cur
+
+  COMPREPLY=()   # Array variable storing the possible completions.
+  cur=${COMP_WORDS[COMP_CWORD]}
+
+  case "$cur" in
+    -*)
+    COMPREPLY=( $( compgen -W '-e -l -q -v -h -V \
+                               --edit --summary --version --verbose \
+                               --dry-run --dump --clear-shell-args \
+                               --evaluate --highlight --init --list \
+                               --no-highlight --quiet --help' -- $cur ) );;
+#   Generate the completion matches and load them into $COMPREPLY array.
+    *)
+    COMPREPLY=($(compgen -W "$(just --summary)" "$cur"));;
+  esac
+
+  return 0
 }
 
 complete -F _just_completions just
-
